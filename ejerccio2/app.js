16270c4ed3d19953678f8a8d3bd015878c21f3cd
michaelbenavides3 <michaelfb803@gmail.com>
2025-12-09 19:34:02 -0500

    agrege el ejercicio #2

--- ejerccio2/app.js
+++ ejerccio2/app.js
@@ -1,35 +1,47 @@
-/*
-tabla de multiplicar utilizando un while para que el usuario dese consultar otra tabla, 
-va hasta el numero 12
-*/
+
+// tabla de multiplicar utilizando un while para que el usuario dese consultar otra tabla, 
+// va hasta el numero 12
+
 
 import pregunta from "prompt-sync";
 
-console.log("bienvenido a la tabla de multiplicar.\n Ingrese 1- si desea conultar una tabla.\n 2-para salir: ");
+console.log("bienvenido a la tabla de multiplicar.\n Ingrese\n 1- si desea conultar una tabla.\n 2-para salir: ");
 
 const prompt = pregunta();
 
-let opcion = parseInt(prompt("Que opcoion desea ingresar (1 o 2): "))
 
-switch (opcion) {
+let continuar = true;
+
+while (continuar) {
+
+    let opcion = parseInt(prompt("Que opcoion desea ingresar (1 o 2): "))
     
+    switch (opcion) {
+        
+        
+        
+        case 1:
+            //console.log("Ingrese el numero de la tabla: ");
+            let tabla = parseInt(prompt("Ingrese el numero de la tabla: "))
+
+            for (let i = 1 ; i <= 12 ; i ++){
+                console.log(`${tabla} x ${i} = ${tabla * i }`);
+                
+            }    
+            break;
+        case 2:
+            console.log("usted esta saliendo del programaa. ");
+            continuar = false;
+            break;
     
+        default:
+            console.log("opcions invalida");
+            
+            break;
+    }
     
-    case 1:
-        //console.log("Ingrese el numero de la tabla: ");
-        let tabla = parseInt(prompt("Ingrese el numero de la tabla: "))
-
-        for (let  tabla = 1;  ; numero <= 12 ; i++){
-            for ()
-        }    
-        break;
-    case 2:
-
-        break;
-
-    default:
-        break;
 }
+
 
 
 console.log("ejercicio2");
